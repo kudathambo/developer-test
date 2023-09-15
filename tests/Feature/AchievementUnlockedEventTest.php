611b2a33c5a1ce99dc1709b054335cd50d9a92a4
Kudakwashe Thambo <kudathambo@gmail.com>
2023-09-16 00:08:13 +0200

    Added achievement unloacked is dispatched test case

--- tests/Feature/AchievementUnlockedEventTest.php
+++ tests/Feature/AchievementUnlockedEventTest.php
@@ -2,13 +2,22 @@
 
 namespace Tests\Feature;
 
+use App\Events\AchievementUnlocked;
+use App\Events\CommentWritten;
+use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
 class AchievementUnlockedEventTest extends TestCase
 {
     public function testAchievementUnlockedIsDispatched(){
-
+        Event::fake();
+        \event(new AchievementUnlocked('First Comment Written', User::factory()->create()));
+        Event::assertDispatched(AchievementUnlocked::class, function ($e){
+          return  $e->achievement = 'First Comment Written';
+        });
     }
 }
