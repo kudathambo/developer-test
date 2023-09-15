e69b6f5e44f2290dfff5a8c978bc2519f6624744
Kudakwashe Thambo <kudathambo@gmail.com>
2023-09-15 23:58:17 +0200

    Added the Comment Listener Logic

--- app/Listeners/CommentListener.php
+++ app/Listeners/CommentListener.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners;
 
+use App\Events\AchievementUnlocked;
 use App\Events\CommentWritten;
 use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -25,5 +26,27 @@ class CommentListener
         $comment = $event->comment;
         $user = $comment->user;
         $commentsCount = $user->comments->count();
+        $achievementUnlocked = "";
+        switch ($commentsCount){
+            case 1:
+                $achievementUnlocked = 'First Comment Written';
+                break;
+            case 3:
+                $achievementUnlocked = '3 Comments Written';
+                break;
+            case 5:
+                $achievementUnlocked = '5 Comments Written';
+                break;
+            case 10:
+                $achievementUnlocked = '10 Comments Written';
+                break;
+            case 20:
+                $achievementUnlocked = '20 Comments Written';
+                break;
+        }
+
+        if(!empty($achievementUnlocked)){
+            event(new AchievementUnlocked($achievementUnlocked, $user));
+        }
     }
 }
