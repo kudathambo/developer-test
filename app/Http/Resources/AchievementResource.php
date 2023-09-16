e0e89b239adc6c42912331566b97bea1ec8327c6
Kudakwashe Thambo <kudathambo@gmail.com>
2023-09-16 17:53:13 +0200

    Test json response when 3 Comments are achieved

--- app/Http/Resources/AchievementResource.php
+++ app/Http/Resources/AchievementResource.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Resources;
 
+use App\Events\BadgeUnlocked;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Event;
 
 class AchievementResource extends JsonResource
 {
@@ -18,27 +20,33 @@ class AchievementResource extends JsonResource
         $unlocked = [];
         $lessonsWatched = [];
         $commentsWritten = [];
+        $badgesAvailable = badgesAvailable();
+        $commentAchievements = commentsWritten();
+        $lessonAchievements = lessonsWatched();
         $achievementsCount = $this->achievements->count();
         foreach ($this->achievements as $achievement){
                 $unlocked[] = $achievement->name;
         }
         foreach ($unlocked as $eventUnlocked){
-            if(in_array($eventUnlocked,commentsWritten())){
+            if(in_array($eventUnlocked, $commentAchievements)){
                 $commentsWritten[] = $eventUnlocked;
-            }else if(in_array($eventUnlocked, lessonsWatched())){
+            }else if(in_array($eventUnlocked, $lessonAchievements)){
                 $lessonsWatched[] = $eventUnlocked;
             }
         }
         $currentBadge = $this->badges->last();
-        $nextBadges = array_filter(badgesAvailable(),function($badge) use($currentBadge){
+        if($currentBadge == null){
+            $currentBadge = $this->badges()->create(['name' => $badgesAvailable[0]]);
+        }
+        $nextBadges = array_filter($badgesAvailable,function($badge) use($currentBadge){
             return $badge != $currentBadge->name;
         });
         $nextBadge = array_shift($nextBadges);
-        $nextBadgeIndex = (int) array_search($nextBadge, badgesAvailable());
-        $nextCommentAchievement = array_filter(commentsWritten(), function($comment) use($commentsWritten){
+        $nextBadgeIndex = (int) array_search($nextBadge, $badgesAvailable);
+        $nextCommentAchievement = array_filter($commentAchievements, function($comment) use($commentsWritten){
             return ! in_array($comment, $commentsWritten);
         });
-        $nextLessonAchievement = array_filter(lessonsWatched(), function($lesson) use($lessonsWatched){
+        $nextLessonAchievement = array_filter($lessonAchievements, function($lesson) use($lessonsWatched){
             return ! in_array($lesson, $lessonsWatched);
         });
          return [
--- tests/Feature/CommentsEventTest.php
+++ tests/Feature/CommentsEventTest.php
@@ -6,9 +6,11 @@ use App\Events\CommentWritten;
 use App\Listeners\CommentListener;
 use App\Models\Comment;
 use App\Models\User;
+use Database\Factories\CommentFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class CommentsEventTest extends TestCase
@@ -21,6 +23,30 @@ class CommentsEventTest extends TestCase
         Event::assertDispatched(CommentWritten::class, function($e) use($comment){
             return $e->comment->id == $comment->id;
         });
+    }
+
+    public  function  testThreeCommentsAchieved(){
+        $user = User::factory()->create();
+        for ( $i = 0 ; $i <=3 ; $i++ ){
+           $this->writeComment($user->id);
+        }
+        $response = $this->get("/users/{$user->id}/achievements");
+        $response->assertJson(function (AssertableJson $json) {
+        return $json->where('data.current_badge', 'Beginner')
+            ->where('data.unlocked_achievements', function($u){
+                return $u->contains('First Comment Written') && $u->contains('3 Comments Written');
+            })
+            ->where('data.next_badge', 'Intermediate')
+            ->where('data.remaining_to_unlock_next_badge', 2)
+            ->etc();
+        });
+    }
+
+    private function writeComment($uid){
+        $comment = CommentFactory::new(['user_id' => $uid])->create();
+        $event = new CommentWritten($comment);
+        $listener = new CommentListener();
+        $listener->handle($event);
     }
 
 }
