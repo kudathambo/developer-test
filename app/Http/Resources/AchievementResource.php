a3129d32919bae6e952a3ad0c1f5a31146e9ed89
Kudakwashe Thambo <kudathambo@gmail.com>
2023-09-16 08:43:00 +0200

    Updated Achievement Resource

--- app/Http/Resources/AchievementResource.php
+++ app/Http/Resources/AchievementResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
 
 class AchievementResource extends JsonResource
 {
@@ -15,16 +16,37 @@ class AchievementResource extends JsonResource
     public function toArray(Request $request): array
     {
         $unlocked = [];
+        $lessonsWatched = [];
+        $commentsWritten = [];
+        $achievementsCount = $this->achievements->count();
         foreach ($this->achievements as $achievement){
                 $unlocked[] = $achievement->name;
         }
-        return [
+        foreach ($unlocked as $eventUnlocked){
+            if(in_array($eventUnlocked,commentsWritten())){
+                $commentsWritten[] = $eventUnlocked;
+            }else if(in_array($eventUnlocked, lessonsWatched())){
+                $lessonsWatched[] = $eventUnlocked;
+            }
+        }
+        $currentBadge = $this->badges->last();
+        $nextBadges = array_filter(badgesAvailable(),function($badge) use($currentBadge){
+            return $badge != $currentBadge->name;
+        });
+        $nextBadge = array_shift($nextBadges);
+        $nextBadgeIndex = (int) array_search($nextBadge, badgesAvailable());
+        $nextCommentAchievement = array_filter(commentsWritten(), function($comment) use($commentsWritten){
+            return ! in_array($comment, $commentsWritten);
+        });
+        $nextLessonAchievement = array_filter(lessonsWatched(), function($lesson) use($lessonsWatched){
+            return ! in_array($lesson, $lessonsWatched);
+        });
+         return [
                 'unlocked_achievements' => $unlocked,
-                'next_available_achievements' => [],
-                'current_badge' => '',
-                'next_badge' => '',
-                'remaining_to_unlock_next_badge' => 0
-
+                'next_available_achievements' => [array_shift($nextCommentAchievement), array_shift($nextLessonAchievement)],
+                'current_badge' => $currentBadge->name,
+                'next_badge' => $nextBadge,
+                'remaining_to_unlock_next_badge' => ($nextBadgeIndex - $achievementsCount)
         ];
     }
 }
